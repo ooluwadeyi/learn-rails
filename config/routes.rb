1492cbf05c23935e0f1cd2a820045d0becf05a7f
Olasunkanmi Oluwadeyi <sunkanmi.laplace@gmail.com>
2016-03-30 23:38:40 +0100

    Dynamic Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
-Rails.application.routes.draw do
+LearnRails::Application.routes.draw do
+
+      root to: 'visitors#new'
+
+end       
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +57,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
