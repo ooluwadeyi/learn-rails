7cca9418b7baae8fe48761d027121fbd8e46eaf6
Olasunkanmi Oluwadeyi <sunkanmi.laplace@gmail.com>
2016-04-06 10:14:50 +0100

    add 'about' and 'contact' pages

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,9 +2,7 @@ class VisitorsController < ApplicationController
 
 	def new
 		Rails.logger.debug "DEBUG: entering new method"
-		@owner = Owner.new
-		flash.now[:notice] = 'Welcome'
-		flash.now[:alert] = 'My birthday is soon'
+		@owner = Owner.new		
 		Rails.logger.debug "DEBUG: Owner name is " + @owner.name
 	
 	end
