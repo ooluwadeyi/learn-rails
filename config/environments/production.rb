e7249e27ab51910547bb27c1375c523f152235ec
Sunkanmi <sunkanmi.laplace@gmail.com>
2016-05-25 22:35:01 +0100

    made perform delivery error false in enviroment production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_delivery_errors = true
+  #config.action_mailer.perform_delivery_errors = true
   config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
